ebdaa8648762b5642d32a86118754fdcbd38dda2
Lixtronn <113902485+Lixtronn@users.noreply.github.com>
2022-09-19 18:57:40 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "Lix Software | Key System", HidePremium = false, SaveConfig = false, IntroEnabled = false})
+local Window = OrionLib:MakeWindow({Name = "Lix Hub | Key System", HidePremium = false, SaveConfig = false, IntroEnabled = false})
 
 OrionLib:MakeNotification({
 	Name = "Logged In!",
