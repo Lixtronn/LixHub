4cf192f492d722fce176a1e2b01025f10a11d7b8
Lixtronn <113902485+Lixtronn@users.noreply.github.com>
2022-09-30 17:05:15 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "Lix Hub | Key System", HidePremium = false, SaveConfig = false, IntroEnabled = false})
+local Window = OrionLib:MakeWindow({Name = "Lix Software | Key System", HidePremium = false, SaveConfig = false, IntroEnabled = false})
 
 OrionLib:MakeNotification({
 	Name = "Logged In!",
@@ -13,16 +13,42 @@ _G.Key = "L"
 _G.KeyInput = "string"
 
 function MakeScript()
-    if game.PlaceId == 6490016198 then
+    if game.PlaceId == 6490016198 then -- demonslayer tycoon
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtronn/Scripts/main/demonslayer.lua"))()
         OrionLib:Destroy()
+
+        if game.PlaceId == 2753915549 then -- bloxfruits
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtronn/Scripts/main/scripthub_bloxfruits.lua"))()
+            OrionLib:Destroy() 
+
+            if game.PlaceId == 2809202155 then -- YBA
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtronn/Scripts/main/scripthub_yba.lua"))()
+                OrionLib:Destroy() 
+
+                if game.PlaceId == 8396586868 then -- one piece game
+                    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtronn/Scripts/main/scripthub_a_one_piece_game.lua"))()
+                    OrionLib:Destroy() 
+
+                    if game.PlaceId == 6461766546 then -- a hero destiny
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtronn/Scripts/main/scripthub_a_heros_destiny.lua"))()
+                        OrionLib:Destroy()
+
+                        if game.PlaceId == 5130598377 then -- aut
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtronn/Scripts/main/scripthub_aut.lua"))()
+                            OrionLib:Destroy()
+                        
     else
         OrionLib:MakeNotification({
             Name = "Game Not Supported!",
             Content = ":/",
             Image = "rbxassetid://4483345998",
             Time = 5
-        })    
+        })
+    end
+    end
+    end
+    end   
+    end
     end    
 end
 
