6350c7c4ffaefcc6f0b8c6d6636fbe9cca3b6608
Lixtronn <113902485+Lixtronn@users.noreply.github.com>
2022-09-30 17:02:41 +0200

    Create scripthub_a_one_piece_game.lua

---
+++ scripthub_a_one_piece_game.lua
@@ -0,0 +1,37 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Lix Hub | Script Hub A One Piece Game", HidePremium = false, IntroEnabled = true, IntroText = "Lix Hub"})
+
+--Variables
+local player = game.Players.LocalPlayer
+
+local Tab = Window:MakeTab({
+	Name = "Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "LeadMarker Hub",
+	Callback = function()
+		loadstring(game:HttpGet("https://rawscripts.net/raw/Testing-A-0ne-Piece-Game-LeadMarkerAOPG-1595"))()
+
+end    
+})
+
+
+local Tab = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddLabel("Credits to everyone who made these hubs")
+
+Tab:AddButton({
+	Name = "My Discord Server! (click to copy link)",
+	Callback = function()
+		setclipboard("https://discord.com/invite/RECcSvEXrW")
+end    
+})
+OrionLib:Init()
