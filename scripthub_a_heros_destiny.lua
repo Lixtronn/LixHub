47bbbcbfe3f85d9ab2f752d46c673109274a8996
Lixtronn <113902485+Lixtronn@users.noreply.github.com>
2022-09-30 17:04:32 +0200

    Create scripthub_a_heros_destiny.lua

---
+++ scripthub_a_heros_destiny.lua
@@ -0,0 +1,37 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Lix Hub | Script Hub A Hero's Destiny", HidePremium = false, IntroEnabled = true, IntroText = "Lix Hub"})
+
+--Variables
+local player = game.Players.LocalPlayer
+
+local Tab = Window:MakeTab({
+	Name = "Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Shit Boy Hub",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/Solx69/Shit-Boy-Hub-Main/main/AHD'))()
+
+end    
+})
+
+
+local Tab = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddLabel("Credits to everyone who made these hubs")
+
+Tab:AddButton({
+	Name = "My Discord Server! (click to copy link)",
+	Callback = function()
+		setclipboard("https://discord.com/invite/RECcSvEXrW")
+end    
+})
+OrionLib:Init()
