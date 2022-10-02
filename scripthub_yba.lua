83ca77d76a78dfaf0894dd008935d2e3ebcc75bb
Lixtronn <113902485+Lixtronn@users.noreply.github.com>
2022-10-02 19:25:07 +0200

    Update scripthub_yba.lua

--- scripthub_yba.lua
+++ scripthub_yba.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Lix Hub | Script Hub YBA", HidePremium = false, IntroEnabled = true, IntroText = "Lix Hub"})
+local Window = OrionLib:MakeWindow({Name = "Mewisous Hub | YBA", HidePremium = false, IntroEnabled = true, IntroText = "Lix Hub"})
 
 --Variables
 local player = game.Players.LocalPlayer
